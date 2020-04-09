3854249467f0b172a4f9d93c6d06b116cfe0651a
Sergi Navarro <serginavarro.ro@gmail.co>
2020-04-09 16:30:13 +0100

    First commit
    Build folder only

---
+++ build/precache-manifest.76b3905fc5f60a83ceb4c6bd7e2fd536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863c9ab4944ad9fecd67519c0cac313c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2309d1759dea82d94a5",
+    "url": "/static/css/main.eef8494a.chunk.css"
+  },
+  {
+    "revision": "f3deac67952e8859378c",
+    "url": "/static/js/2.adb47fe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.adb47fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2309d1759dea82d94a5",
+    "url": "/static/js/main.c4332825.chunk.js"
+  },
+  {
+    "revision": "c180d28527a6a9fdbc70",
+    "url": "/static/js/runtime-main.df5589d7.js"
+  }
+]);
